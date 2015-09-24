870738182aa8056698730838c00764d3aeb7dab5
umebosh <gsd.strike.f.007.031@gmail.com>
2015-09-25 01:06:05 +0900

    SDカード用の雛形作成

---
+++ _10DoF2SD/_10DoF2SD.ino
@@ -0,0 +1,65 @@
+#include <SD.h>
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_BMP085_U.h>
+#include <Adafruit_L3GD20_U.h>
+#include <Adafruit_10DOF.h>
+
+/* Assign a unique ID to the sensors */
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
+Adafruit_L3GD20_Unified       gyro  = Adafruit_L3GD20_Unified(20);
+
+const int chipSelect = 4;
+
+void setup(){
+  Serial.begin(9600);
+  Serial.print("Initializing SD card...");
+  pinMode(10,OUTPUT);
+  if(!SD.begin(chipSelect)){
+      Serial.println("Card failed, or not present");
+      return;
+  }
+  Serial.println("card initialized.");
+  
+  Serial.println("");
+
+  Serial.print("Initializing sensors...");
+    /* Initialise the sensors */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
+    while(1);
+  }
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+  if(!bmp.begin())
+  {
+    /* There was a problem detecting the BMP085 ... check your connections */
+    Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  if(!gyro.begin())
+  {
+    /* There was a problem detecting the L3GD20 ... check your connections */
+    Serial.print("Ooops, no L3GD20 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  Serial.println("sensors initialized.");
+
+}
+
+void loop(){
+  String dataString = "";
+  
+  
+  
+}
