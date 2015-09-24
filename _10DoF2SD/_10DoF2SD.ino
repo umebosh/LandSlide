8890e25d4a373945e6da40290ec41218c809fe09
umebosh <gsd.strike.f.007.031@gmail.com>
2015-09-25 01:48:50 +0900

    10DoFのデータ読み取り部分とSDカードへの書き込み処理作成

--- _10DoF2SD/_10DoF2SD.ino
+++ _10DoF2SD/_10DoF2SD.ino
@@ -14,6 +14,7 @@ Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
 Adafruit_L3GD20_Unified       gyro  = Adafruit_L3GD20_Unified(20);
 
 const int chipSelect = 4;
+int times = 0; //経過時間を記録
 
 void setup(){
   Serial.begin(9600);
@@ -54,12 +55,76 @@ void setup(){
     while(1);
   }
   Serial.println("sensors initialized.");
+  Serial.println("");
+
+  Serial.println("times,ax,ay,az,mx,my,mz,gx,gy,gz,press,temp");    //書き込みデータ一覧
+
 
 }
 
 void loop(){
-  String dataString = "";
+  String data = "";      //データ保存用の変数
+  
+  //経過時間を記録
+  sensors_event_t event;
+  data += times;
+  data += ",";
+  
+  //加速度を記録
+  accel.getEvent(&event);
+  data += event.acceleration.x;
+  data += ",";
+  data += event.acceleration.y;
+  data += ",";
+  data += event.acceleration.z;
+  data += ",";
+  
+  //地磁気を記録
+  mag.getEvent(&event);
+  data += event.magnetic.x;
+  data += ",";
+  data += event.magnetic.y;
+  data += ",";
+  data += event.magnetic.z;
+  data += ",";
+  
+  //ジャイロを記録
+  gyro.getEvent(&event);
+  data += event.gyro.x;
+  data += ",";
+  data += event.gyro.y;
+  data += ",";
+  data += event.gyro.z;
+  data += ",";
+  
+  //気圧と気温を記録
+  bmp.getEvent(&event);
+  if (event.pressure)
+  {
+    data += event.pressure;
+    data += ",";
+    
+    float temperature;
+    bmp.getTemperature(&temperature);   
+    data += temperature;
+  }
+  
+
   
   
+  //SDカードへの書き込み処理
+  File logData = SD.open("logData.csv", FILE_WRITE);
   
+  //ファイルが存在すれば書き込み
+  if(logData){
+    logData.println(data);
+    Serial.println(data);
+    logData.close ();
+  }
+  else{
+        Serial.println("error opening logData.csv");
+  }
+  
+  delay(1000);
+  times += 1;
 }
