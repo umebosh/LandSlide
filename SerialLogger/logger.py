2cefa83cdd69d09dde5341ff8f0c87b34c4f1949
umebosh <gsd.strike.f.007.031@gmail.com>
2015-09-28 06:24:42 +0900

    時間を取得

--- SerialLogger/logger.py
+++ SerialLogger/logger.py
@@ -3,13 +3,20 @@ __author__ = 'nonakanaoki'
 import serial
 import os
 import sys
+import datetime
 
-module_path = os.path.abspath(os.path.dirname(__file__)) + "/.." #モジュールを読み込むためのを指定
-sys.path.append(module_path)
-
+module_path = os.path.abspath(os.path.dirname(__file__)) + "/.."  # モジュールを読み込むためのを指定
+sys.path.append(module_path)  # これでモジュールの読み込みができるようになる
 from Dialog.serial_open_dialog import Dialog
 
 dialog = Dialog
+
+
+def get_time():  # 時間が入ったlistを返す
+    d = datetime.datetime.today()
+    time = [d.year, d.month, d.day, d.hour, d.minute, d.second, d.microsecond]
+    return time
+
 
 if __name__ == "__main__":
     dialog.set_usb_port()
