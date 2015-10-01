ef37fa130be28c8a2be9db78f6cb7d17f09162ca
umebosh <gsd.strike.f.007.031@gmail.com>
2015-10-01 09:05:00 +0900

    アスキーコマンドから変換 ver.nonaka

--- Exam/decode.py
+++ Exam/decode.py
@@ -1,34 +1,53 @@
 # coding: UTF-8
 __author__ = 'nonakanaoki'
 # :00A012FF00020010004210002F00095410044800014810535700000300001100000203F34903F349X
-s = '00000010003E00000F000A00000B5A101136106E3D00000300001300000303F32803F328X'
+# s = '00000010020F000358000862001851101509103B1210041410042F10030403F353001C3C'
+s = "000900100027100023000A00100724102A2D10611800000300001100000103F16203F162"
 li = list(s)
 data = ''
 datalist = []
-hexnum = ""
 print(len(li))
 for i in range(len(li)):
-    if (i)%6 != 0:
+    if (i) % 6 != 0:
         data += li[i]
     else:
         if i != 0:
             datalist.append(data)
         data = li[i]
 
-# datalist.append(data) ##データが3バイト未満の最後のところ
+datalist.append(data) ##データが3バイト未満の最後のところ
 print(datalist)
 
-numlist = []
-for i in range(len(datalist)):
-    n = datalist[i]
-    tmp = [int(n[0], 16), int(n[1:4], 16), int(n[4:6], 16)]
+def div_data(data):
+    li = list(data)
+    divide_data = ''
+    datalist = []
+    print(len(li))
+    for i in range(len(li)):
+        if (i) % 6 != 0:
+            divide_data += li[i]
+        else:
+            if i != 0:
+                datalist.append(divide_data)
+            divide_data = li[i]
 
-    result = float(tmp[1] + tmp[2] * 0.01)
+datalist.append(data) ##データが3バイト未満の最後のところ
+print(datalist)
 
-    if tmp[0] == 1:
-        result *= -1
 
-    numlist.append(result)
+def decode(list):
+    numlist = []
+    for i in range(len(list)):
+        n = list[i]
+        tmp = [int(n[0], 16), int(n[1:4], 16), int(n[4:6], 16)]
 
-print(numlist)
+        result = float(tmp[1] + tmp[2] * 0.01)
 
+        if tmp[0] == 1:
+            result *= -1
+
+        numlist.append(result)
+    return numlist
+
+
+print(decode(datalist))
---
+++ Exam/decode2.py
@@ -0,0 +1,58 @@
+# coding: UTF-8
+__author__ = 'nonakanaoki'
+
+# 01 A0 12 8100ECCA FFFFFFFF 8A 0024 000A0010001B100027000960100709102A1B10611800000300001100000103F16203F162D8
+# まずヘッダー部分を切り出してチェックサムの部分（おしりの1バイト）を消す
+# つぎにそれを6バイトづつに切り出す
+# 規則に従って復号化
+
+s = ':01A0128100ECCAFFFFFFFF930024000900100027100023000A00100724102A2D10611800000300001100000103F16203F162FA'
+
+
+# 送られてきた生データの最初の：とチェックサムを排除したstrを返す関数
+# s = ejection_data_part(s)
+#  実際はバイトで来るからそこの変換の各必要がある？
+def ejection_data_part(args):
+    args = args[1:-2]
+    if type(args) == bytes: args.decode('utf-8')
+    return args
+
+
+# 13,14バイト目を見て正しいデータが送られてきてるかを確認
+# 正しいなら[子機番号,lqi,data,data,data,...]の形でlistを返す
+# 正しくない場合emptyのlistを返す
+def divide_data(args):
+    # 子機番号： 最初の１バイト
+    # LQI    ： 12バイト目
+    # データ長 ： 13,14バイト目
+    li = []
+    if args[26:28] == '24':  # バイト数 これが２４なら正しいデータが来てる
+        li.append(int(args[:2], 16))  # 送信元
+        li.append(int(args[22:24], 16))  # LQI 16進数から１０進数に直しておく
+
+        args = args[28:]  # ヘッダー部分を切り捨てる
+
+        temp = []
+        for i in range(12):  # 3バイトずつに分割して１０進数に変換したものをリストに入れる
+            li.append(decode_data(args[:6]))
+            args = args[6:]
+
+    return li
+
+
+# 受け取ったアスキー形式の文字列を元の形に戻す
+def decode_data(args):
+    intg = float(int(args[1:4], 16))
+    dsml = float(int(args[4:6], 16)) / 100
+    num = intg + dsml
+
+    if args[0] == "1":  # 正負の判定
+        num *= -1
+    return num
+
+
+if __name__ == "__main__":
+    data = s
+    data = ejection_data_part(s)
+    data_list = divide_data(data)
+    print(data_list)
