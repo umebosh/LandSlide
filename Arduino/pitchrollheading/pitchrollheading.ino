e3d7a1087d49838d289857cdf89b80a7ebc7ac80
umebosh <gsd.strike.f.007.031@gmail.com>
2015-11-04 07:13:22 +0900

    ボーレートの変更

---
+++ Arduino/pitchrollheading/pitchrollheading.ino
@@ -0,0 +1,59 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_10DOF.h>
+
+/* Assign a unique ID to the sensors */
+Adafruit_10DOF                dof   = Adafruit_10DOF();
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
+Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+
+void initSensors()
+{
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println(F("Ooops, no LSM303 detected ... Check your wiring!"));
+    while(1);
+  }
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+}
+
+void setup(void)
+{
+  Serial.begin(115200);  
+  /* Initialise the sensors */
+  initSensors();
+}
+
+void loop(void)
+{
+  sensors_event_t accel_event;
+  sensors_event_t mag_event;
+  sensors_vec_t   orientation;
+
+  /* Calculate pitch and roll from the raw accelerometer data */
+  accel.getEvent(&accel_event);
+  if (dof.accelGetOrientation(&accel_event, &orientation))
+  {
+    Serial.print(orientation.roll);
+    Serial.print(F(","));
+    Serial.print(orientation.pitch);
+    Serial.print(F(","));
+  }
+  
+  /* Calculate the heading using the magnetometer */
+  mag.getEvent(&mag_event);
+  if (dof.magGetOrientation(SENSOR_AXIS_Z, &mag_event, &orientation))
+  {
+    Serial.print(orientation.heading);
+  }  
+  
+  Serial.println(F(",e"));
+  delay(100);
+}
